46bc32174189836bc557b0a2a8758d7f1a8b9fab
lesego-dev <71130812+lesego-dev@users.noreply.github.com>
2026-02-16 14:24:50 +0200

    Update city.js

--- city.js
+++ city.js
@@ -7,12 +7,13 @@ const cityInfo = async (city) => {
     
     //alternatives for the fetch() call, including headers with an API key and host
     const alternatives = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'dfc14bccf0msh55cd09095fc7e5bp1087b2jsn142626d6d73d',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-      }
-    };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  }
+};
+
     
 
         //call fetch() to retrieve data from the API, the parse the response as JSON
@@ -63,4 +64,5 @@ const cityInfo = async (city) => {
     
     //cal function to retrieve information about Bloemfontein
     cityInfo('Polokwane')
+
     getWeather(23.8962 ,29.4486)
